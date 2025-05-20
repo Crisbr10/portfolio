1a8bea65bb93a570328bd88df272c2cca8b94d8a
Crisbr10 <borgescristhian1@gmail.com>
2025-05-20 20:08:47 +0200

    Tailwindcss configurado y modificaciones iniciales en la pagina inicial

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./public/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: {
+          900: "#061026",
+          800: "#123277",
+          700: "#1e54c7",
+          500: "#5483e6",
+          200: "#b3c8f4",
+          50: "#fdfeff",
+        },
+      },
+    },
   },
   plugins: [],
 };
